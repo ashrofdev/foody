7e033fa3e8c2081292aaebf2aa05e83f20a60c38
Samuel <sammykay301@live.com>
2021-11-30 17:00:33 +0100

    added logo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {FaSearch} from 'react-icons/fa'
+import logo from '../resources/images/logo.png'
 const Hero = () => {
     return (
         <>
         <div className="hero-container">
             <div className="hero-image">
+                <div className="logo">
+                    <img src={logo} alt="foody" width="50%"/>
+
+                </div>
                 <form>
                     <input className="form-control search" placeholder="Search Recipe"/>
                     <span id="search" >
